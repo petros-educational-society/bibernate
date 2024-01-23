46a5612a815a32ba41265f9a25c2b35b7d283e93
OleksiiSkachkov <skachkov@kodaris.com>
2024-01-23 11:41:02 +0200

    added dataSource

--- src/main/java/com/petros/bibernate/Bibernate.java
+++ src/main/java/com/petros/bibernate/Bibernate.java
@@ -1,7 +1,11 @@
 package com.petros.bibernate;
 
+import javax.sql.DataSource;
+
+import static com.petros.bibernate.config.AppConfig.initializeDataSource;
+
 public class Bibernate {
     public static void main(String[] args) {
-
+        DataSource dataSource = initializeDataSource();
     }
 }
