62cf7a732ac2e08ca36a4ef0b8d365821b8bc0ab
OleksiiSkachkov <skachkov@kodaris.com>
2024-01-23 11:24:01 +0200

    added maven

---
+++ src/main/java/com/petros/bibernate/Bibernate.java
@@ -0,0 +1,7 @@
+package com.petros.bibernate;
+
+public class Bibernate {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/petros/bibernate/BibernateTest.java
@@ -0,0 +1,6 @@
+package com.petros.bibernate;
+
+
+class BibernateTest {
+
+}
